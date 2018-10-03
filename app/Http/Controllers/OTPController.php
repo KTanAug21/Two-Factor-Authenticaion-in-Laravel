644bbba22481c6f55a02f5d5a3a028323e4f63a5
Kathryn Tan <ktan.developer@gmail.com>
2018-10-03 10:27:31 +0300

    Add OTP Expiration Checker and Handler

--- app/Http/Controllers/OTPController.php
+++ app/Http/Controllers/OTPController.php
@@ -10,52 +10,67 @@ use Session;
 use App\User;
 class OTPController extends Controller
 {
-    public function validateOTP(Request $request)
-    {
-        //$this->checkExpired(Auth::user()->otp_expiration);
-        // Check if otp field is set
-        $request->validate([
-            'otp' => 'required',
-        ]);
-       
-        // Session is otp-verified when entry matches with stored data
-        if($request->input('otp') == Auth::user()->otp){  
-            // Change otp_expiration to the present with added minutes from session life, signalling verified otp           
-            $user = Auth::user();
-            $user->otp_expiration = \Carbon\Carbon::now()->addMinutes(config('session.lifetime'));
-            $user->save();  
-                 
-            //Set session as otp ok
-            Session::put('otp_verified', true);
-            return redirect('/home')->with('success_login',"Welcome back.");
-        } else {
-            return redirect('/otp_form')->with('error_otp', 'Incorrect code.');
-        }
-    }
-
-
+    
     public function showForm()
     {   
+        // Cant access if already otp verified
+        if(Session::get('otp_verified') )
+        {
+            return redirect('/home');
+        }
+        
         return view('auth.otp_form');
     }  
    
-
-
-    
-    // Helper functions
-    public function send_message($user)
+    public function validateOTP(Request $request)
     {
-        $data = array(
-            'otp' => $user->otp,
-            'user' => $user
-        );
-        Mail::send('auth.otp_message', $data, function($message) use($user){
-            $message->from('noreplies@just.no', 'What did I tell you.');
-            $message->to($user->email, $user->name)->subject('Get your code.');
-        });
+        $user = Auth::user();
+        // Only allow otp validation with valid otp session
+        if($this->checkExpired(Auth::user()->otp_expiration))
+        {
+        // Expired OTP
+            // Delete OTP 
+            $user->otp_expiration = null;
+            $user->otp = null;
+            $user->save();
+            // Delet User Credentials
+            Auth::logout();
+            // Redirect to login
+            return redirect('/login')->with('session_entry_exp', "Entry Session Expired!");
+        }else{
+        // OTP not expired
+            // Check if otp field is set
+            $request->validate([
+                'otp' => 'required',
+            ]);
+        
+            // Session is otp-verified when entry matches with stored data
+            if($request->input('otp') == Auth::user()->otp){  
+                // Change otp_expiration to the present with added minutes from session life, signalling verified otp           
+                $user->otp_expiration = \Carbon\Carbon::now()->addMinutes(config('session.lifetime'));
+                $user->save();  
+                    
+                //Set session as otp ok
+                Session::put('otp_verified', true);
+                return redirect('/home')->with('success_login',"Welcome back.");
+            } else {
+                return redirect('/otp_form')->with('error_otp', 'Incorrect code.');
+            }
+        }
     }
     public function reSend()
     {
+        // Cant access if already otp verified
+        if(Session::get('otp_verified') )
+        {
+            return redirect('/home');
+        }
+        // Verify credentials first!
+        if(Auth::user()==null)
+        {
+            return redirect('/login');
+        }
+
         // Change user otp and otp_exp
         $user = Auth::user();
         $user->otp_expiration = \Carbon\Carbon::now();
@@ -71,4 +86,31 @@ class OTPController extends Controller
         return redirect('otp_form')->with('otp_resend','OTP has been resent to your mail!');
     }
 
+    // Helper functions
+    public function send_message($user)
+    {
+        $data = array(
+            'otp' => $user->otp,
+            'user' => $user
+        );
+        Mail::send('auth.otp_message', $data, function($message) use($user){
+            $message->from('noreplies@just.no', 'What did I tell you.');
+            $message->to($user->email, $user->name)->subject('Get your code.');
+        });
+    }    
+    public function checkExpired($otp_expiration)
+    {
+        // Get time difference between present and time otp was added
+        $time = \Carbon\Carbon::now();
+        $diff = $time->diffInMinutes($otp_expiration);
+
+        // OTP Expires after 25 minutes
+        if($diff>=10)   
+        {
+            return true;    // Expired
+        }else
+        {
+            return false;   // Still valid
+        }
+    }
 }
